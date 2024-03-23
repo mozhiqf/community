b54aa9816c9a0c7c0ae6aa382a80fd0eacfcdc52
cyl <Cyl221002907@bjfu.edu.cn>
2024-03-23 15:31:20 +0800

    生成登录验证码

--- src/main/java/com/forum/community/controller/LoginController.java
+++ src/main/java/com/forum/community/controller/LoginController.java
@@ -2,6 +2,8 @@ package com.forum.community.controller;
 
 import com.forum.community.entity.User;
 import com.forum.community.service.UserService;
+import com.google.code.kaptcha.Producer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 import static com.forum.community.util.CommunityConstant.ACTIVATION_REPEAT;
@@ -18,6 +26,9 @@ import static com.forum.community.util.CommunityConstant.ACTIVATION_SUCCESS;
 public class LoginController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private Producer kaptchaProducer;
 
     @RequestMapping(path = "/register", method = RequestMethod.GET)
     public String getRegisterPage() {
@@ -29,8 +40,8 @@ public class LoginController {
         return "/site/login";
     }
 
-    @RequestMapping(path = "/register",method = RequestMethod.POST)
-    public String register(Model model, User user){
+    @RequestMapping(path = "/register", method = RequestMethod.POST)
+    public String register(Model model, User user) {
         Map<String, Object> map = userService.register(user);
         if (map == null || map.isEmpty()) {
             model.addAttribute("msg", "注册成功,我们已经向您的邮箱发送了一封激活邮件,请尽快激活!");
@@ -59,6 +70,20 @@ public class LoginController {
             model.addAttribute("target", "/index");
         }
         return "/site/operate-result";
+    }
+
+    @RequestMapping(path = "/kaptcha", method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response, HttpSession session) {
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+        session.setAttribute("kaptcha", text);
+        response.setContentType("image/png");
+        try {
+            OutputStream os = response.getOutputStream();
+            ImageIO.write(image, "png", os);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
